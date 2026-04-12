b505989b7a3bf23edf7a47c8bacdb79db51f49c1
shivahari04 <shivahari04@yahoo.com>
2026-04-11 22:51:55 -0400

    Updated Caeser_cipher

--- Caeser_Cipher.py
+++ Caeser_Cipher.py
@@ -10,3 +10,27 @@ a string text
 an integer shift
 """
 
+def caesar_cipher(text,shift):
+    answer = ""
+    
+    for char in text:
+        if char.isalpha():
+            
+            #To determine if we start from 'A' or 'a'
+            letter = ord('A') if char.isupper() else ord('a')
+            shifted_letter = chr((ord(char) - letter + shift) % 26 + letter)
+            answer += shifted_letter
+        
+        else:
+            answer +=char
+    
+    return answer   
+
+print("\nEncoding Caeser_Cipher!\n")
+user_input = input("Enter your message: ")
+try:
+    user_shift = int(input("\n Enter your shift value (default is 3): "))
+    encoded_message = caesar_cipher(user_input, user_shift)
+    print(f"Encoded message: {encoded_message}")
+except ValueError:
+    print("Value Error - Please input correct value!!!")
